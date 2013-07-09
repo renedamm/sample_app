7b15e7b312581b90951bbb69936123083329e654
Rene Damm <rene.damm@gmx.net>
2013-07-10 01:09:55 +0200

    Added titles to the static pages.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+
   end
 
   describe "Help page" do
@@ -18,6 +23,11 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+
   end
 
   describe "About page" do
@@ -25,6 +35,11 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
 
   end
