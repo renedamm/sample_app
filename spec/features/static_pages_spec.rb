72d4a209239eef8cc227a6dc64accf78ea55e49d
Rene Damm <rene.damm@gmx.net>
2013-07-10 02:13:19 +0200

    Refactored title string tests for static pages.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let (:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have h1 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_title('Ruby on Rails Tutorial Sample App | Home')
+      page.should have_title("#{base_title} | Home")
     end
 
   end
@@ -25,7 +27,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_title('Ruby on Rails Tutorial Sample App | Help')
+      page.should have_title("#{base_title} | Help")
     end
 
   end
@@ -39,7 +41,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_title('Ruby on Rails Tutorial Sample App | About Us')
+      page.should have_title("#{base_title} | About Us")
     end
 
   end
@@ -53,7 +55,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      page.should have_title('Ruby on Rails Tutorial Sample App | Contact')
+      page.should have_title("#{base_title} | Contact")
     end
 
   end
