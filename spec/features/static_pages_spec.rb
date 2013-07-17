c60d71a0e1a0bb834f0184264174949caaa647f4
Rene Damm <rene.damm@gmx.net>
2013-07-18 00:14:55 +0200

    Added /signup route.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -46,4 +46,17 @@ describe "Static pages" do
 
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_title(full_title("About Us"))
+    click_link "Help"
+    page.should have_title(full_title("Help"))
+    click_link "Contact"
+    page.should have_title(full_title("Contact"))
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_title(full_title("Sign up"))
+  end
+
 end
