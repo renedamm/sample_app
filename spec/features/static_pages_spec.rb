a03d82f6ff1c02e0b7ad865f72e4b3f94451e492
Rene Damm <rene.damm@gmx.net>
2013-07-17 03:12:31 +0200

    Changed routing of static pages.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -7,17 +7,17 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_title("| Home")
     end
 
@@ -26,12 +26,12 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_title("#{base_title} | Help")
     end
 
@@ -40,12 +40,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_title("#{base_title} | About Us")
     end
 
@@ -54,12 +54,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_title("#{base_title} | Contact")
     end
 
