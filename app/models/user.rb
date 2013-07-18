4cc040362fcd88c7d5934906a4fbddfcf42b9c13
Rene Damm <rene.damm@gmx.net>
2013-07-19 01:18:15 +0200

    Added uniqueness constraint to user email validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w\-.+]+@[a-z\d\-.]+\.[a-z]{2,}\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email,
+            presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
 end
