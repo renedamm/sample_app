4e0cc193c6939028b30422353b49a6c73663631d
Rene Damm <rene.damm@gmx.net>
2013-07-21 03:55:34 +0200

    Started work on signup form.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -9,6 +9,7 @@ describe "UserPages" do
 
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title(full_title('Sign up')) }
+
   end
 
   describe "profile page" do
@@ -17,6 +18,34 @@ describe "UserPages" do
 
     it { should have_selector('h1', text: user.name) }
     it { should have_title(full_title(user.name)) }
+  end
+
+  describe "signup" do
+
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        old_count = User.count
+        click_button "Create my account"
+        new_count = User.count
+        new_count.should == old_count
+      end
+    end
+
+    describe "with valid information" do
+      it "should create a user" do
+        old_count = User.count
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+        click_button "Create my account"
+        new_count = User.count
+        new_count.should == old_count + 1
+      end
+    end
+
   end
 
 end
