17165d3de3fd745734bea64a9aa46e8276b1eaad
Rene Damm <rene.damm@gmx.net>
2013-07-21 02:37:14 +0200

    Added user gravatar support on profile page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Return Gravatar or user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravator_url = "http://gravatar.com/avatar/#{gravatar_id}"
+    return image_tag(gravator_url, alt: user.name, class: "gravatar")
+  end
+
 end
