c802e593d8b2a2868166b5eea6817826df8dd53a
Rene Damm <rene.damm@gmx.net>
2013-07-21 02:00:55 +0200

    Starting to add content to user profile page.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -9,7 +9,14 @@ describe "UserPages" do
 
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title(full_title('Sign up')) }
+  end
 
+  describe "profile page" do
+    let (:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', text: user.name) }
+    it { should have_title(full_title(user.name)) }
   end
 
 end
