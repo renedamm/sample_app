9604ac95303862055a549540360ca1c300969648
Rene Damm <rene.damm@gmx.net>
2013-07-23 01:01:01 +0200

    Made Gravatar on user profile smaller.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   # Return Gravatar or user.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 } )
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravator_url = "http://gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravator_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     return image_tag(gravator_url, alt: user.name, class: "gravatar")
   end
 
