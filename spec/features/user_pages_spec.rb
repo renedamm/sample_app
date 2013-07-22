13f92197ded72496e9026a0a2197971b063bc2b4
Rene Damm <rene.damm@gmx.net>
2013-07-22 22:57:49 +0200

    Test for sign up form error messages.

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -30,6 +30,11 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "should print error messages" do
+        before { click_button submit }
+        it { should have_selector('div', text: /The form contains [0-9]+ errors./) }
+      end
     end
 
     describe "with valid information" do
