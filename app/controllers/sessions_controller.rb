9eacbc73bb77a530fa1ead6fe74f151c3dc867b7
Rene Damm <rene.damm@gmx.net>
2013-07-24 23:49:46 +0200

    Added sign in form.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,10 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
+  def destroy
+  end
+
 end
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -4,11 +4,22 @@ describe "AuthenticationPages" do
 
   subject { page }
 
-  describe "signin page" do
+  describe "sign in page" do
     before { visit signin_path }
 
     it { should have_title('Sign in') }
     it { should have_selector('h1', text: 'Sign in') }
+  end
+
+  describe "sign in" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', 'Invalid' )}
+    end
   end
 
 end
